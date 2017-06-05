c0354b7e3987f7fa7915678569e48257b05a80c9
ranjit <kumarranjitg@gmail.com>
2017-06-06 01:58:27 +0530

    ...

--- models/user.js
+++ models/user.js
@@ -10,13 +10,13 @@ var user = {
         return db.query("select * from user where Id=?", [id], callback);  
     },  
     addUser: function(user, callback) {  
-        return db.query("Insert into user values(?,?,?)", [user.Id, user.Title, user.Status], callback);  
+        return db.query("Insert into user values(?,?,?)", [user.id, user.email, user.pwd], callback);  
     },  
     deleteUser: function(id, callback) {  
         return db.query("delete from user where Id=?", [id], callback);  
     },  
     updateUser: function(id, user, callback) {  
-        return db.query("update user set Title=?,Status=? where Id=?", [user.Title, user.Status, id], callback);  
+        return db.query("update user set email=?,pwd=? where Id=?", [user.email, user.pwd, id], callback);  
     }  
 };  
 module.exports = user; 
